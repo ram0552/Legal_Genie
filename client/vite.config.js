b1aa12737adbadd5f4146ba96d05eaf73489f5ea
ram0552 <152691387+ram0552@users.noreply.github.com>
2026-01-27 15:37:03 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             '/api': {
-                target: 'http://localhost:5000',
+                target: 'https://gen-ai-1-dcba.onrender.com',
                 changeOrigin: true
             }
         }
